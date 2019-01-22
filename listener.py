e89236693d61278c1286b4b9f745e52245b582a0
Emin Mastizada <emin@mastizada.com>
2019-01-22 04:16:29 +0300

    Add event listeners

--- listener.py
+++ listener.py
@@ -4,3 +4,41 @@ Telegram Client
 
 Listens to Telegram Events.
 """
+from telethon import TelegramClient, events
+from utils import Settings, get_chats
+import logging
+
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
+settings = Settings()
+CHATS = []
+
+
+client = TelegramClient(
+    session='sessions/app.session',
+    api_id=settings.telegram.api_id,
+    api_hash=settings.telegram.api_hash
+)
+
+
+@client.on(events.userupdate.UserUpdate(chats=CHATS))
+def user_update(event):
+    if not isinstance(event.online, bool):
+        # no status info
+        return
+    if event.online:
+        # online user
+        # id: event.chat.id
+        pass
+    elif event.last_seen:
+        # offline user
+        # id: event.chat.id
+        # last_seen: datetime
+        pass
+
+
+if __name__ == '__main__':
+    CHATS = get_chats(settings)
+    client.start()
+    client.run_until_disconnected()
