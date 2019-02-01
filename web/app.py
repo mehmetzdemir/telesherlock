b2200d7953706345534e1a215c6647fcd8ce9ebd
Mehmet Özdemir <mehmetozdemir@Mehmets-MacBook-Air.local>
2019-02-01 23:18:40 +0300

    bootsrap ve charts eklendi

--- web/app.py
+++ web/app.py
@@ -4,11 +4,17 @@ Flask Web
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return render_template("index.html", number=10, number2=20)
+    return render_template("index.html")
 
+@app.route("/charts")
+def charts():
+    data = {
+        'aybuke_online': [7.0, 6.9, 9.1, 14.5, 18.4, 21.5, 25.2],
+        'halil_online': [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0]
+    }
+    return render_template("chart.html", **data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
